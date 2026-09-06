4df6e46467b0d9429a4530e1e53e1650d7b8ccf7
Davidestevansouza-rgb <davidestevansouza@gmail.com>
2026-09-06 16:23:02 -0400

    Sincronizar carga estética del seguimiento en www

--- www/js/modules/enhancements-final-20260906.js
+++ www/js/modules/enhancements-final-20260906.js
@@ -1,5 +1,6 @@
 import './correcciones-estables-20260906.js';
 import './correcciones-ultimas-20260906.js';
+import './seguimiento-estetica-20260906.js';
 import { state, todayISO } from '../state.js';
 import { fmtMoney, fmtDate, clienteNombre, chipPago } from '../ui.js';
 import { escHtml } from '../sanitize.js';
