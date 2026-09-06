9edc4bcee41920b14943f17b936eed1a1346ba61
Davidestevansouza-rgb <davidestevansouza@gmail.com>
2026-09-06 13:15:53 -0400

    fix: load exact-selector definitive corrections

--- js/modules/enhancements-final-20260906.js
+++ js/modules/enhancements-final-20260906.js
@@ -1,5 +1,6 @@
 import './correcciones-preview-20260906.js';
 import './correcciones-funcionales-20260906.js';
+import './correcciones-definitivas-20260906.js';
 import { state, todayISO } from '../state.js';
 import { fmtMoney, fmtDate, clienteNombre, chipPago } from '../ui.js';
 import { escHtml } from '../sanitize.js';
