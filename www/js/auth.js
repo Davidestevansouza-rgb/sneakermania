ffb6f1b106e8b971a4a862ada96df68462338ced
Davidestevansouza-rgb <davidestevansouza@gmail.com>
2026-09-20 00:20:38 -0400

    perf: reduce idle realtime and legacy photo polling (www)

--- www/js/auth.js
+++ www/js/auth.js
@@ -7,7 +7,7 @@ import { showToast, logActivity, setConnStatus } from './ui.js';
 import * as db from './db.js';
 import { startNotificationSync, stopNotificationSync } from './modules/notificaciones.js';
 import { startRealtimeAgenda, stopRealtimeAgenda } from './modules/agenda.js';
-import { startRealtimeConfig, stopRealtimeConfig } from './modules/configuracion.js';
+import { stopRealtimeConfig } from './modules/configuracion.js';
 import { hasPin, saveSessionForPin, getSavedSession, clearPin } from './pin.js';
 import { biometricDisponible, registrarBiometria, verificarBiometria, hasBiometric, biometricOfferAnswered, ofrecerActivarBiometria, saveBiometricSession, getBiometricSession } from './biometric.js';
 
@@ -368,7 +368,6 @@ export async function onAuthenticated(authUser) {
   // Cada módulo ya es idempotente: no crea canales/timers duplicados.
   startNotificationSync();
   startRealtimeAgenda();
-  startRealtimeConfig();
   return true;
 }
 
